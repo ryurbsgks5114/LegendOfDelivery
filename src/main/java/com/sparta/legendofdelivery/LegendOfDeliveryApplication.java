f72770291cb92cba1fd11a1411acd3079442d6b4
ryurbsgks5114 <ryurbsgks5114@naver.com>
2024-06-19 15:17:25 +0900

    project init

---
+++ src/main/java/com/sparta/legendofdelivery/LegendOfDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.legendofdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegendOfDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegendOfDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/legendofdelivery/LegendOfDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.legendofdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegendOfDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
