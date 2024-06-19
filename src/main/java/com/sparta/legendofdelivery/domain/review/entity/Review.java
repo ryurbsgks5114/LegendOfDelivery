5a6547280dacfe8cc585ed4ab0597422ad822c6f
KIM <wltn9169@naver.com>
2024-06-19 20:18:54 +0900

    🎉 add review 엔티티 생성, Timestamped 생성

---
+++ src/main/java/com/sparta/legendofdelivery/domain/review/entity/Review.java
@@ -0,0 +1,41 @@
+package com.sparta.legendofdelivery.domain.review.entity;
+
+import com.sparta.legendofdelivery.domain.user.entity.User;
+import com.sparta.legendofdelivery.global.entity.Timestamped;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Review extends Timestamped {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String content;
+
+//  @ManyToOne(fetch = FetchType.LAZY)
+//  @JoinColumn(name = "user_id")
+//  private User user;
+
+//  @ManyToOne(fetch = FetchType.LAZY)
+//  @JoinColumn(name = "store_id")
+//  private Store store;
+
+//  @OneToMany(mappedBy = "schedule", cascade = CascadeType.ALL, orphanRemoval = true)
+//  private List<Like> likeList = new ArrayList<>();
+
+}
