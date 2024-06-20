fa2fd2d3c1316de5471ac0f99efd9a053471968e
hyojin <hyojinwjd99@naver.com>
2024-06-20 14:46:37 +0900

    🩹 fix: LikeRepository @Repository 어노테이션 추가

--- src/main/java/com/sparta/legendofdelivery/domain/like/repository/LikeRepository.java
+++ src/main/java/com/sparta/legendofdelivery/domain/like/repository/LikeRepository.java
@@ -2,6 +2,8 @@ package com.sparta.legendofdelivery.domain.like.repository;
 
 import com.sparta.legendofdelivery.domain.like.entity.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LikeRepository extends JpaRepository<Like,Long> {
 }
