ef936d54cbd9398df80a4987697f2a8edb35bbe4
hyojin <hyojinwjd99@naver.com>
2024-06-20 16:01:42 +0900

    fix: user entity 연결 + 생성자 추가

--- src/main/java/com/sparta/legendofdelivery/domain/like/entity/Like.java
+++ src/main/java/com/sparta/legendofdelivery/domain/like/entity/Like.java
@@ -1,6 +1,7 @@
 package com.sparta.legendofdelivery.domain.like.entity;
 
 import com.sparta.legendofdelivery.domain.review.entity.Review;
+import com.sparta.legendofdelivery.domain.user.entity.User;
 import com.sparta.legendofdelivery.global.entity.Timestamped;
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -15,11 +16,16 @@ public class Like extends Timestamped {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-//    @ManyToOne
-//    @JoinColumn(name = "user_id")
-//    private User user;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
     @ManyToOne
     @JoinColumn(name = "review_id")
     private Review review;
+
+    public Like(Long id, User user, Review review) {
+        this.id = id;
+        this.review = review;
+    }
 }
