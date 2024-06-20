077c1af03af0f8553c1eb58b3f713c8358740cc6
nenney <a01056499749@gmail.com>
2024-06-20 16:22:23 +0900

    feat: enum 클래스 생성

--- src/main/java/com/sparta/legendofdelivery/domain/order/entity/OrderStatusEnum.java
+++ src/main/java/com/sparta/legendofdelivery/domain/order/entity/OrderStatusEnum.java
@@ -1,5 +1,10 @@
 package com.sparta.legendofdelivery.domain.order.entity;
 
 public enum OrderStatusEnum {
+    ACCEPTANCE("ACCEPTANCE"),
+    COMPLETE("COMPLETE");
 
+    private String status;
+
+    OrderStatusEnum(String type) { this.status = status; }
 }
