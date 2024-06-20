2af7fc78e6c06583767147a571a9da35b1854828
ryurbsgks5114 <ryurbsgks5114@naver.com>
2024-06-20 20:27:04 +0900

    update: response 수정

---
+++ src/main/java/com/sparta/legendofdelivery/global/dto/DataResponse.java
@@ -0,0 +1,18 @@
+package com.sparta.legendofdelivery.global.dto;
+
+import lombok.Getter;
+
+@Getter
+public class DataResponse<T> {
+
+    private int statusCode;
+    private String message;
+    private T data;
+
+    public DataResponse(int statusCode, String message, T data) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
+    }
+
+}
