ffa539a4f9d2598ca7e2140127d1b868483fea4d
ryurbsgks5114 <ryurbsgks5114@naver.com>
2024-06-20 21:00:21 +0900

    update: enum 위치 변경

--- src/main/java/com/sparta/legendofdelivery/domain/user/service/UserService.java
+++ src/main/java/com/sparta/legendofdelivery/domain/user/service/UserService.java
@@ -2,10 +2,10 @@ package com.sparta.legendofdelivery.domain.user.service;
 
 import com.sparta.legendofdelivery.domain.user.dto.UserSignupRequestDto;
 import com.sparta.legendofdelivery.domain.user.entity.User;
+import com.sparta.legendofdelivery.domain.user.entity.UserOauth;
+import com.sparta.legendofdelivery.domain.user.entity.UserRole;
+import com.sparta.legendofdelivery.domain.user.entity.UserStatus;
 import com.sparta.legendofdelivery.domain.user.repository.UserRepository;
-import com.sparta.legendofdelivery.global.enumeration.UserOauth;
-import com.sparta.legendofdelivery.global.enumeration.UserRole;
-import com.sparta.legendofdelivery.global.enumeration.UserStatus;
 import com.sparta.legendofdelivery.global.exception.BadRequestException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
