76d9a830ba94f77753dbdc28d75327202e75051d
Ham-Seung-Wan <syham001@naver.com>
2024-06-21 21:00:04 +0900

    category 가격 추가

--- src/main/java/com/sparta/legendofdelivery/domain/store/entity/Category.java
+++ src/main/java/com/sparta/legendofdelivery/domain/store/entity/Category.java
@@ -1,10 +1,19 @@
 package com.sparta.legendofdelivery.domain.store.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum Category {
 
-    KOREAN,
-    PIZZA,
-    CHINA,
-    BURGER
+    KOREAN(15000),
+    PIZZA(20000),
+    CHINA(12000),
+    BURGER(10000);
+
+    private final int price;
+
+    Category(int price) {
+        this.price = price;
+    }
 
 }
