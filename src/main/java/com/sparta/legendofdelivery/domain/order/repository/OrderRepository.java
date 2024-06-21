89223c54f0155e5b2eef90ca4de8e761b2606b1b
KIM <wltn9169@naver.com>
2024-06-22 00:31:24 +0900

    🎉 add 리뷰 생성

--- src/main/java/com/sparta/legendofdelivery/domain/order/repository/OrderRepository.java
+++ src/main/java/com/sparta/legendofdelivery/domain/order/repository/OrderRepository.java
@@ -2,11 +2,12 @@ package com.sparta.legendofdelivery.domain.order.repository;
 
 import com.sparta.legendofdelivery.domain.order.dto.OrderResponseDto;
 import com.sparta.legendofdelivery.domain.order.entity.Order;
+import com.sparta.legendofdelivery.domain.store.entity.Store;
 import com.sparta.legendofdelivery.domain.user.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository <Order, Long > {
-
+  int countByUserAndStore(User user, Store store);
 }
--- src/main/java/com/sparta/legendofdelivery/domain/review/controller/ReviewController.java
+++ src/main/java/com/sparta/legendofdelivery/domain/review/controller/ReviewController.java
@@ -1,16 +1,35 @@
 package com.sparta.legendofdelivery.domain.review.controller;
 
 
+import com.sparta.legendofdelivery.domain.review.dto.ReviewRequestDto;
+import com.sparta.legendofdelivery.domain.review.dto.ReviewResponseDto;
 import com.sparta.legendofdelivery.domain.review.service.ReviewService;
+import com.sparta.legendofdelivery.global.dto.DataResponse;
+import com.sparta.legendofdelivery.global.security.UserDetailsImpl;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/review")
+@RequestMapping("api/reviews")
 @RequiredArgsConstructor
 public class ReviewController {
 
   private final ReviewService reviewService;
 
+  @PostMapping
+  public ResponseEntity<DataResponse<ReviewResponseDto>> createReview(
+      @Valid @RequestBody ReviewRequestDto requestDto
+      , @AuthenticationPrincipal UserDetailsImpl userDetails
+  ) {
+    ReviewResponseDto responseDto = reviewService.createReview(requestDto, userDetails.getUsername());
+
+    return ResponseEntity.ok().body(new DataResponse<>(201, "리뷰 생성에 성공했습니다.", responseDto)
+    );
+  }
 }
