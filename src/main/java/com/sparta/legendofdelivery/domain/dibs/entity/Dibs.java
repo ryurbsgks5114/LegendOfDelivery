27612da936907b9a8d776f630cb2835f4a49d9e0
hyojin <hyojinwjd99@naver.com>
2024-06-22 17:48:42 +0900

    add: Dibs entity 추가

---
+++ src/main/java/com/sparta/legendofdelivery/domain/dibs/entity/Dibs.java
@@ -0,0 +1,31 @@
+package com.sparta.legendofdelivery.domain.dibs.entity;
+
+import com.sparta.legendofdelivery.domain.store.entity.Store;
+import com.sparta.legendofdelivery.domain.user.entity.User;
+import com.sparta.legendofdelivery.global.entity.Timestamped;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Dibs extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    public Dibs(User user, Store store) {
+        this.user = user;
+        this.store = store;
+    }
+}
