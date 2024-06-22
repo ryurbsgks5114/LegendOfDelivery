38f75253b3511462ed10203cbed1cfee3c254ab1
hyojin <hyojinwjd99@naver.com>
2024-06-22 18:46:00 +0900

    update: 사용자별 목록을 조회하는 기능인 getAllDibsByUser 추가

--- src/main/java/com/sparta/legendofdelivery/domain/dibs/service/DibsService.java
+++ src/main/java/com/sparta/legendofdelivery/domain/dibs/service/DibsService.java
@@ -1,19 +1,27 @@
 package com.sparta.legendofdelivery.domain.dibs.service;
 
+import com.sparta.legendofdelivery.domain.dibs.dto.DibsResponseDto;
 import com.sparta.legendofdelivery.domain.dibs.entity.Dibs;
 import com.sparta.legendofdelivery.domain.dibs.repository.DibsRepository;
 import com.sparta.legendofdelivery.domain.store.entity.Store;
 import com.sparta.legendofdelivery.domain.store.repository.StoreRepository;
 import com.sparta.legendofdelivery.domain.user.entity.User;
+import com.sparta.legendofdelivery.global.dto.DataResponse;
 import com.sparta.legendofdelivery.global.dto.MessageResponse;
 import com.sparta.legendofdelivery.global.exception.BadRequestException;
 import com.sparta.legendofdelivery.global.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class DibsService {
 
+    private static final Logger log = LoggerFactory.getLogger(DibsService.class);
     private final DibsRepository dibsRepository;
     private final StoreRepository storeRepository;
 
@@ -46,6 +54,23 @@ public class DibsService {
             return new MessageResponse(200, "찜 삭제를 성공했습니다.");
         } else {
             throw new NotFoundException("해당 가게는 찜한 가게가 아닙니다.");
+        }
+    }
+
+    public DataResponse<List<DibsResponseDto>> getAllDibsByUser(User user){
+        List<Dibs> dibsList = dibsRepository.findAllByUserId(user.getId());
+
+        List<DibsResponseDto> dibsResponseDtoList = new ArrayList<>();
+
+        if (!dibsList.isEmpty()){
+            for (Dibs dibs : dibsList) {
+                dibsResponseDtoList.add(DibsResponseDto.toDto(dibs));
+            }
+
+            return new DataResponse<>(200, "찜한 가게 목록 조회를 성공했습니다.", dibsResponseDtoList);
+
+        } else {
+            throw new NotFoundException("찜한 가게가 없습니다.");
         }
     }
 
