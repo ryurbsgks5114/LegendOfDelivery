a0f2ccdc8bb48758899258bd93295b98d6216249
hyojin <hyojinwjd99@naver.com>
2024-06-22 17:29:48 +0900

    update: addLike + deleteLike 기능 추가

--- src/main/java/com/sparta/legendofdelivery/domain/like/controller/LikeController.java
+++ src/main/java/com/sparta/legendofdelivery/domain/like/controller/LikeController.java
@@ -1,12 +1,11 @@
 package com.sparta.legendofdelivery.domain.like.controller;
 
 import com.sparta.legendofdelivery.domain.like.service.LikeService;
+import com.sparta.legendofdelivery.domain.user.security.UserDetailsImpl;
+import com.sparta.legendofdelivery.global.dto.MessageResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/likes")
@@ -18,8 +17,17 @@ public class LikeController {
         this.likeService = likeService;
     }
 
-//    @PostMapping("{reviewId}")
-//    public ResponseEntity<CommonResponse<Void>> addLike(@PathVariable Long reviewId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-//
-//    }
+    @PostMapping("{reviewId}")
+    public ResponseEntity<MessageResponse> addLike(@PathVariable Long reviewId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        return ResponseEntity.ok(likeService.addLike(reviewId, userDetails.getUser()));
+
+    }
+
+    @DeleteMapping("{reviewId}")
+    public ResponseEntity<MessageResponse> deleteLike(@PathVariable Long reviewId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        return ResponseEntity.ok(likeService.deleteLike(reviewId, userDetails.getUser()));
+
+    }
 }
