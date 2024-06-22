258790c2271fb5a7017303f8d8ce256e01ad4f8e
KIM <wltn9169@naver.com>
2024-06-22 15:19:22 +0900

    🎉 add 가게별 리뷰 목록 조회

--- src/main/java/com/sparta/legendofdelivery/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/sparta/legendofdelivery/domain/review/repository/ReviewRepository.java
@@ -3,9 +3,12 @@ package com.sparta.legendofdelivery.domain.review.repository;
 import com.sparta.legendofdelivery.domain.review.entity.Review;
 import com.sparta.legendofdelivery.domain.store.entity.Store;
 import com.sparta.legendofdelivery.domain.user.entity.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+  List<Review> findByUserAndStore(User user, Store store);
 
   int countByUserAndStore(User user, Store store);
 }
