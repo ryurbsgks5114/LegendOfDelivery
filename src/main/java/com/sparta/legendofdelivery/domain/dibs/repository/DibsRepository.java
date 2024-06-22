46631a3b761896dcf537feecfe058c8444707ae3
hyojin <hyojinwjd99@naver.com>
2024-06-22 18:12:30 +0900

    update: findByStoreIdAndUserId 메소드 추가

--- src/main/java/com/sparta/legendofdelivery/domain/dibs/repository/DibsRepository.java
+++ src/main/java/com/sparta/legendofdelivery/domain/dibs/repository/DibsRepository.java
@@ -4,7 +4,9 @@ import com.sparta.legendofdelivery.domain.dibs.entity.Dibs;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DibsRepository extends JpaRepository<Dibs, Long> {
-
+    Optional<Dibs> findByStoreIdAndUserId(long storeId, long userId);
 }
