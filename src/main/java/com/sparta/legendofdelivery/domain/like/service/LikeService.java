33fbc5c9fc8be79b385311a89c232eb3a6a614bb
hyojin <hyojinwjd99@naver.com>
2024-06-22 17:29:23 +0900

    update: deleteLike 기능 추가 + 함수 반환 타입 void -> MessageResponse로 교체

--- src/main/java/com/sparta/legendofdelivery/domain/like/service/LikeService.java
+++ src/main/java/com/sparta/legendofdelivery/domain/like/service/LikeService.java
@@ -5,6 +5,7 @@ import com.sparta.legendofdelivery.domain.like.repository.LikeRepository;
 import com.sparta.legendofdelivery.domain.review.entity.Review;
 import com.sparta.legendofdelivery.domain.review.repository.ReviewRepository;
 import com.sparta.legendofdelivery.domain.user.entity.User;
+import com.sparta.legendofdelivery.global.dto.MessageResponse;
 import com.sparta.legendofdelivery.global.exception.BadRequestException;
 import com.sparta.legendofdelivery.global.exception.NotFoundException;
 import org.springframework.stereotype.Service;
@@ -20,14 +21,29 @@ public class LikeService {
         this.reviewRepository = reviewRepository;
     }
 
-    public void addLike(Long reviewId, User user) {
+    public MessageResponse addLike(Long reviewId, User user) {
         Review review = findReviewById(reviewId);
+        if (review.getUser().getId().equals(user.getId())){
+            throw new BadRequestException("본인이 작성한 리뷰에는 좋아요를 할 수 없습니다.");
+        }
         Like checkIsLike = findLikeByReviewIdAndUserId(reviewId, user.getId());
         if (checkIsLike != null) {
             throw new BadRequestException("이미 좋아요를 누른 리뷰입니다.");
         } else {
             Like like = new Like(review, user);
             likeRepository.save(like);
+            return new MessageResponse(200, "좋아요 등록에 성공했습니다.");
+        }
+    }
+
+    public MessageResponse deleteLike(Long reviewId, User user) {
+        findReviewById(reviewId);
+        Like checkIslike = findLikeByReviewIdAndUserId(reviewId, user.getId());
+        if (checkIslike == null) {
+            throw new NotFoundException("해당 리뷰는 좋아요가 등록되어 있지 않습니다.");
+        } else {
+            likeRepository.delete(checkIslike);
+            return new MessageResponse(200, "좋아요 취소를 성공했습니다.");
         }
     }
 
