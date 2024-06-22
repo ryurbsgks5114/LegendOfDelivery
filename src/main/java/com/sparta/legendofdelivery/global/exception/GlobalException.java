d020d3812d164212cb1c1d9ea35125cf25935487
ryurbsgks5114 <ryurbsgks5114@naver.com>
2024-06-22 16:39:15 +0900

    style: 사용하지 않는 import 제거

--- src/main/java/com/sparta/legendofdelivery/global/exception/GlobalException.java
+++ src/main/java/com/sparta/legendofdelivery/global/exception/GlobalException.java
@@ -4,14 +4,10 @@ import com.sparta.legendofdelivery.global.dto.MessageResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j(topic = "Global Exception")
 @RestControllerAdvice
