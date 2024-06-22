5bb81919f6ae57a816811ff1dde3005d461666ed
hyojin <hyojinwjd99@naver.com>
2024-06-22 18:45:01 +0900

    update: 사용자별 찜 목록 가져오는 findAllByUserId method 추가

--- src/main/java/com/sparta/legendofdelivery/domain/dibs/repository/DibsRepository.java
+++ src/main/java/com/sparta/legendofdelivery/domain/dibs/repository/DibsRepository.java
@@ -4,9 +4,11 @@ import com.sparta.legendofdelivery.domain.dibs.entity.Dibs;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface DibsRepository extends JpaRepository<Dibs, Long> {
     Optional<Dibs> findByStoreIdAndUserId(long storeId, long userId);
+    List<Dibs> findAllByUserId(long userId);
 }
